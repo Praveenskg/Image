c07ba7b6e10e1cd13c60d790d14a3f398cc37828
Praveen Singh <Praveensinghiics@gmail.com>
2023-10-06 01:13:27 +0530

    :green_heart: Fixed Some Errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,101 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 const API_URL = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 24;
+
 const App = () => {
   const searchInput = useRef(null);
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [enlargedIndex, setEnlargedIndex] = useState(null);
 
-  const toggleEnlarge = (index) => {
-    setEnlargedIndex(enlargedIndex === index ? null : index);
-  };
+  const categories = [
+    "man",
+    "women",
+    "nature",
+    "birds",
+    "animal",
+    "cat",
+    "Dog",
+    "computer",
+    "house",
+  ];
 
-  useEffect(() => {
-    fetchImages();
-  }, [page]);
-  const fetchImages = async () => {
+  const fetchImages = useCallback(async () => {
     try {
-      const { data } = await axios.get(
-        `${API_URL}?query=${
-          searchInput.current.value
-        }&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
+      if (searchInput.current.value) {
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
 
-      setImages(data.results);
-      setTotalPages(data.total_pages);
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+      }
     } catch (error) {
       console.log(error);
     }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
   };
 
   const handleSearch = (event) => {
     event.preventDefault();
-    fetchImages();
+    resetSearch();
   };
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
+    resetSearch();
   };
 
   return (
     <>
       <h1 className=" text-3xl md:text-4xl   shadow-blue-200 lg:text-5xl text-center font-bold text-dark ">
-        Image Search
+       Image Search
       </h1>
       <form
+        className="w-full max-w-5xl  mx-auto rounded-xl p-12 "
         onSubmit={handleSearch}
-        className="my-8 mx-8 flex space-x-2 justify-center"
       >
-        <input
-          required
-          type="text"
-          className="w-full rounded-md p-3 focus:border-teal-600 border-2 border-teal-400"
-          placeholder="Type something to search..."
-          ref={searchInput}
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white font-bold py-3 px-6 rounded mx-3 hover:bg-teal-400"
-        >
-          Search
-        </button>
+        <div className="my-5 flex">
+          <input
+            required
+            ref={searchInput}
+            type="text"
+            className="w-full rounded-md p-3 focus:border-teal-600 border-2 border-teal-400 capitalize"
+            placeholder="Type something to search..."
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-bold py-3 px-6 rounded mx-3 hover:bg-teal-400"
+          >
+            Search 
+          </button>
+        </div>
       </form>
-      <div className="flex flex-wrap justify-center items-center mx-3 space-x-1 space-y-1 ">
-        <button
-          type="submit"
-          className="inline-block px-6 py-2 border-2  text-white font-medium text-xs leading-tight uppercase rounded-full  hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
-          onClick={() => handleSelection("nature")}
-        >
-          Natures
-        </button>
-        <button
-          type="submit"
-          className="inline-block px-6 py-2 border-2  text-white font-medium text-xs leading-tight uppercase rounded-full  hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
-          onClick={() => handleSelection("birds")}
-        >
-          Birds
-        </button>
-        <button
-          type="submit"
-          className="inline-block px-6 py-2 border-2  text-white font-medium text-xs leading-tight uppercase rounded-full  hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
-          onClick={() => handleSelection("cats")}
-        >
-          cats
-        </button>
-        <button
-          type="button"
-          className="inline-block px-6 py-2 border-2  text-white font-medium text-xs leading-tight uppercase rounded-full  hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
-          onClick={() => handleSelection("man")}
-        >
-          Man
-        </button>
-        <button
-          type="button"
-          className="inline-block px-6 py-2 border-2  text-white font-medium text-xs leading-tight uppercase rounded-full  hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
-          onClick={() => handleSelection("women")}
-        >
-          Women
-        </button>
+      <div className="flex flex-wrap justify-center items-center mx-3 space-x-1 space-y-1">
+        {categories.map((selection, index) => (
+          <button
+            key={index}
+            type="submit"
+            className="inline-block px-6 py-2 border-2 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
+            onClick={() => handleSelection(selection)}
+          >
+            {selection.charAt(0).toUpperCase() + selection.slice(1)}
+          </button>
+        ))}
       </div>
-     
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 p-4">
         {images.map((image, index) => (
           <div
@@ -116,6 +109,24 @@ const App = () => {
             />
           </div>
         ))}
+      </div>
+      <div className="flex justify-center mb-3">
+        {page > 1 && (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+            onClick={() => setPage(page - 1)}
+          >
+            Previous
+          </button>
+        )}
+        {page < totalPages && (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </button>
+        )}
       </div>
     </>
   );
