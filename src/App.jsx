db3e4fe9b342e42ff27e59322d44d15bf132b8cf
Praveen Singh <Praveensinghiics@gmail.com>
2023-10-06 02:02:11 +0530

    :recycle: Fixing Type Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect, useCallback } from "react";
 import axios from "axios";
+import Loader from "./Loader";
 const API_URL = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 24;
 
@@ -8,6 +9,7 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const categories = [
     "man",
@@ -16,14 +18,14 @@ const App = () => {
     "birds",
     "animal",
     "cat",
-    "Dog",
+    "dog",
     "computer",
     "house",
   ];
 
   const fetchImages = useCallback(async () => {
     try {
-      if (searchInput.current.value) {
+      if (searchInput.current && searchInput.current.value) {
         const { data } = await axios.get(
           `${API_URL}?query=${
             searchInput.current.value
@@ -36,13 +38,19 @@ const App = () => {
         setTotalPages(data.total_pages);
       }
     } catch (error) {
-      console.log(error);
+      console.warn(error);
     }
   }, [page]);
 
   useEffect(() => {
     fetchImages();
   }, [fetchImages]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
 
   const resetSearch = () => {
     setPage(1);
@@ -55,79 +63,87 @@ const App = () => {
   };
 
   const handleSelection = (selection) => {
-    searchInput.current.value = selection;
-    resetSearch();
+    if (searchInput.current) {
+      searchInput.current.value = selection;
+      resetSearch();
+    }
   };
 
   return (
     <>
-      <h1 className=" text-3xl md:text-4xl m-1 shadow-blue-200 lg:text-5xl text-center font-bold text-dark ">
-        Image Search
-      </h1>
-      <form
-        className="w-full max-w-5xl  mx-auto rounded-xl p-5 "
-        onSubmit={handleSearch}
-      >
-        <div className="my-5 flex">
-          <input
-            required
-            ref={searchInput}
-            type="text"
-            className="w-full rounded-md p-3 focus:border-teal-600 border-2 border-teal-400 capitalize"
-            placeholder="Type something to search..."
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white font-bold py-3 px-6 rounded mx-3 hover:bg-teal-400"
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="text-3xl md:text-4xl m-1 shadow-blue-200 lg:text-5xl text-center font-bold text-dark">
+            Image Search
+          </h1>
+          <form
+            className="w-full max-w-5xl mx-auto rounded-xl p-5"
+            onSubmit={handleSearch}
           >
-            Search
-          </button>
-        </div>
-      </form>
-      <div className="flex flex-wrap justify-center items-center mx-3 space-x-1 space-y-1">
-        {categories.map((selection, index) => (
-          <button
-            key={index}
-            type="submit"
-            className="inline-block px-6 py-2 border-2 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
-            onClick={() => handleSelection(selection)}
-          >
-            {selection.charAt(0).toUpperCase() + selection.slice(1)}
-          </button>
-        ))}
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 p-4">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="relative overflow-hidden group cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl"
-          >
-            <img
-              src={image.urls.small}
-              alt={image.alt_description}
-              className="w-full h-64 object-cover transition-transform transform group-hover:scale-105 rounded-xl"
-            />
+            <div className="my-5 flex">
+              <input
+                required
+                ref={searchInput}
+                type="text"
+                className="w-full rounded-md p-3 focus:border-teal-600 border-2 border-teal-400 capitalize"
+                placeholder="Type something to search..."
+              />
+              <button
+                type="submit"
+                className="bg-blue-500 text-white font-bold py-3 px-6 rounded mx-3 hover:bg-teal-400"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+          <div className="flex flex-wrap justify-center items-center mx-3 space-x-1 space-y-1">
+            {categories.map((selection, index) => (
+              <button
+                key={index}
+                type="submit"
+                className="inline-block px-6 py-2 border-2 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-teal-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out bg-blue-500"
+                onClick={() => handleSelection(selection)}
+              >
+                {selection.charAt(0).toUpperCase() + selection.slice(1)}
+              </button>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex justify-center mb-3">
-        {page > 1 && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
-            onClick={() => setPage(page - 1)}
-          >
-            Previous
-          </button>
-        )}
-        {page < totalPages && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
-            onClick={() => setPage(page + 1)}
-          >
-            Next
-          </button>
-        )}
-      </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 p-4">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className="relative overflow-hidden group cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl"
+              >
+                <img
+                  src={image.urls.small}
+                  alt={image.alt_description}
+                  className="w-full h-64 object-cover transition-transform transform group-hover:scale-105 rounded-xl"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center mb-3">
+            {page > 1 && (
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+                onClick={() => setPage(page - 1)}
+              >
+                Previous
+              </button>
+            )}
+            {page < totalPages && (
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+                onClick={() => setPage(page + 1)}
+              >
+                Next
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
---
+++ src/Loader.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+const Loader = () => {
+  return (
+    <div className="container">
+      <div className="loader" />
+      <div className="loader" />
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default Loader;
